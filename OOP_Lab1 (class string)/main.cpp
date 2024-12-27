c56828ecaa0931c0f98b77ed5e8402ec79431963
Petr Kolesov <petrkolesov010204@gmail.com>
2024-12-27 23:00:32 +0700

    Revert "usage examplt"
    This reverts commit c3f2404949d5edef323627da1fd6717f750250d1.

--- OOP_Lab1 (class string)/main.cpp
+++ OOP_Lab1 (class string)/main.cpp
@@ -1,20 +1,10 @@
 #include <iostream>
-#include <random>
 
 #include "CStr.h"
 
 int main() {
-	char string[6]{'m','e','l','o','n'};
 	
-	CStr testObject(string);
-
-	std::cout << testObject << std::endl;
-
-	std::cin >> testObject;
-
-	std::cout << testObject << std::endl;
-
-	std::cout << "First symbol : " << testObject[0] << std::endl;
+	std::cout << "Hello world!" << std::endl;
 
 	return 0;
 }

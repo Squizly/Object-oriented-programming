b98f99a3c5d25bfe0f1775f9b2a7039487dfdd20
Petr Kolesov <petrkolesov010204@gmail.com>
2024-12-27 21:09:02 +0700

    add(constructors/destructor)
    * constructor copy
    * constructor with parametr length string
    * constructor with parametr dynamic array char

--- OOP_Lab1 (class string)/CStr.cpp
+++ OOP_Lab1 (class string)/CStr.cpp
@@ -1,3 +1,59 @@
 #include <iostream>
+#include <random>
 
 #include "CStr.h"
+
+char* CStr::generateString(const size_t length)
+{
+	char* tempStr = new char[length + 1];
+
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<> dist(97, 122);
+
+	for (int i = 0; i < length; i++)
+		tempStr[i] = char(dist(gen));
+
+	tempStr[length] = '\0';
+
+	return tempStr;
+}
+
+CStr::CStr()
+{
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<> dist(1, 20);
+
+	size_t length = dist(gen);
+	m_string = generateString(length);
+}
+
+CStr::CStr(size_t& length)
+{
+	// task condition "length <= 20 symbols"
+	if (length > 20) {
+		std::cerr << "CStr::CStr(size_t& length) : length >= 20, value changed to 20" << std::endl;
+		length = 20;
+	}
+	m_string = generateString(length);
+}
+
+CStr::CStr(const char* sourceString)
+{
+	m_string = new char[strlen(sourceString) + 1];
+	strcpy(m_string, sourceString);
+}
+
+CStr::CStr(const CStr& otherObject)
+{
+	m_string = new char(strlen(otherObject.m_string) + 1 );
+	strcpy(m_string, otherObject.m_string);
+
+}
+
+
+CStr::~CStr()
+{
+	delete[] m_string;
+}
--- OOP_Lab1 (class string)/CStr.h
+++ OOP_Lab1 (class string)/CStr.h
@@ -1,9 +1,22 @@
 #pragma once
 
-class CStr {
+class CStr
+{
+	char* generateString(size_t length);
 public:
+
 	CStr();
+	CStr(size_t& length);
+	CStr(const char* sourceString);
+	CStr(const CStr& otherObject);
 	~CStr();
+	
+	void tempPrint()
+	{
+		for (int i = 0; i < strlen(m_string); i++)
+			std::cout << m_string[i];
+		std::cout << std::endl;
+	}
 
 private:
 	char* m_string = nullptr;

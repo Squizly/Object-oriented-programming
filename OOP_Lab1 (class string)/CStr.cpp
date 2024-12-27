ebf3b04f3e7f8e232624cb2be6c11f04acfdd29c
Petr Kolesov <petrkolesov010204@gmail.com>
2024-12-27 22:59:13 +0700

    add(methods, refactor) : operators and method get length

--- OOP_Lab1 (class string)/CStr.cpp
+++ OOP_Lab1 (class string)/CStr.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <assert.h>
 #include <random>
 
 #include "CStr.h"
 
-char* CStr::generateString(const size_t length)
+char* CStr::generateString(const int length)
 {
 	char* tempStr = new char[length + 1];
 
@@ -25,17 +26,14 @@ CStr::CStr()
 	std::mt19937 gen(rd());
 	std::uniform_int_distribution<> dist(1, 20);
 
-	size_t length = dist(gen);
+	int length = dist(gen);
 	m_string = generateString(length);
 }
 
-CStr::CStr(size_t& length)
+CStr::CStr(int length)
 {
 	// task condition "length <= 20 symbols"
-	if (length > 20) {
-		std::cerr << "CStr::CStr(size_t& length) : length >= 20, value changed to 20" << std::endl;
-		length = 20;
-	}
+	assert(length >= 0 && length <= 20);
 	m_string = generateString(length);
 }
 
@@ -47,13 +45,117 @@ CStr::CStr(const char* sourceString)
 
 CStr::CStr(const CStr& otherObject)
 {
-	m_string = new char(strlen(otherObject.m_string) + 1 );
+	m_string = new char[strlen(otherObject.m_string) + 1 ];
 	strcpy(m_string, otherObject.m_string);
-
 }
-
 
 CStr::~CStr()
 {
 	delete[] m_string;
 }
+
+CStr& CStr::operator = (const CStr& otherObject)
+{
+	if (this != &otherObject) {
+
+		delete[]m_string;
+
+		m_string = new char[strlen(otherObject.m_string) + 1];
+		strcpy(m_string, otherObject.m_string);
+	}
+
+	return *this;
+}
+
+CStr& CStr::operator = (const char* string)
+{
+	if (m_string != string) {
+
+		delete[]m_string;
+
+		m_string = new char[strlen(string) + 1];
+		strcpy(m_string, string);
+	}
+
+	return *this;
+}
+
+CStr CStr::operator + (const CStr& otherObject) const
+{
+	int newLength = strlen(m_string) + strlen(otherObject.m_string);
+	CStr tempString(newLength);
+
+	for (int i = 0, j = 0; i < newLength; i++) {
+
+		if (i < strlen(m_string))
+			tempString.m_string[i] = m_string[i];
+
+		else {
+			tempString.m_string[i] = otherObject.m_string[j];
+			j++;
+		}
+	}
+
+	return tempString;
+}
+
+CStr& CStr::operator += (const CStr& otherObject)
+{
+	*this = *this + otherObject;
+
+	return *this;
+}
+
+bool CStr::operator > (const CStr& otherObject) const
+{
+	return strcmp(m_string, otherObject.m_string) > 0;
+}
+
+bool CStr::operator < (const CStr& otherObject) const
+{
+	return strcmp(m_string, otherObject.m_string) < 0;
+}
+
+bool CStr::operator >= (const CStr& otherObject) const
+{
+	return strcmp(m_string, otherObject.m_string) >= 0;
+}
+
+bool CStr::operator <= (const CStr& otherObject) const
+{
+	return strcmp(m_string, otherObject.m_string) <= 0;
+}
+
+char& CStr::operator [] (const int& index) const
+{
+	assert(index >= 0 && index < strlen(m_string));
+	return m_string[index];
+}
+
+int CStr::getLength() const
+{
+	return strlen(m_string);
+}
+
+std::ostream& operator << (std::ostream& stream, const CStr& object)
+{
+	stream << "[ ";
+
+	for (int i = 0; i < object.getLength(); i++)
+		stream << object[i];
+
+	stream << " ]";
+
+	return stream;
+}
+
+std::istream& operator >> (std::istream& stream, CStr& object)
+{
+	char buffer[1024];
+	std::cin >> buffer;
+
+	object = buffer;
+
+	return stream;
+}
+
--- OOP_Lab1 (class string)/CStr.h
+++ OOP_Lab1 (class string)/CStr.h
@@ -2,22 +2,38 @@
 
 class CStr
 {
-	char* generateString(size_t length);
+	friend std::ostream& operator << (std::ostream& stream, const CStr& object);
+	friend std::istream& operator >> (std::istream& stream, CStr& object);
+
 public:
 
 	CStr();
-	CStr(size_t& length);
-	CStr(const char* sourceString);
+	CStr(int length);
+	CStr(const char* arr);
 	CStr(const CStr& otherObject);
 	~CStr();
-	
-	void tempPrint()
-	{
-		for (int i = 0; i < strlen(m_string); i++)
-			std::cout << m_string[i];
-		std::cout << std::endl;
-	}
+
+	CStr& operator = (const CStr& );
+	CStr& operator = (const char* );
+
+	CStr operator + (const CStr& ) const;
+	CStr& operator += (const CStr& );
+	 
+	bool operator > (const CStr& ) const;
+	bool operator < (const CStr& ) const;
+	bool operator >= (const CStr& ) const;
+	bool operator <= (const CStr& ) const;
+
+	char& operator [] (const int& index) const;
+
+	int getLength() const;
 
 private:
+	char* generateString(int length);
+
 	char* m_string = nullptr;
 };
+
+std::ostream& operator << (std::ostream& stream, const CStr& object);
+
+std::istream& operator >> (std::istream& stream, CStr& object);

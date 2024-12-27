fd376f8154a20d61ddca3f63055e30b0a516130f
Petr Kolesov <petrkolesov010204@gmail.com>
2024-12-27 19:03:26 +0700

    add(directory) : for work on the first lab
    * Add class CStr (container for symbols)

---
+++ OOP_Lab1 (class string)/CStr.h
@@ -0,0 +1,10 @@
+#pragma once
+
+class CStr {
+public:
+	CStr();
+	~CStr();
+
+private:
+	char* m_string = nullptr;
+};
---
+++ OOP_Lab1 (class string)/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include "CStr.h"
+
+int main() {
+	
+	std::cout << "Hello world!" << std::endl;
+
+	return 0;
+}

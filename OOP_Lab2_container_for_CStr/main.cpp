b95ebd7a80592179e542c57518e483ed6ec84f51
Petr Kolesov <petrkolesov010204@gmail.com>
2024-12-28 15:53:47 +0700

    usage example

--- OOP_Lab2_container_for_CStr/main.cpp
+++ OOP_Lab2_container_for_CStr/main.cpp
@@ -2,9 +2,28 @@
 
 #include "CStrArray.h"
 
-int main() {
-	
-	std::cout << "Hello world!" << std::endl;
+int main(void) {
+	CStrArray cont(5);
+
+	CStr first;
+	CStr second;
+	CStr third;
+	CStr thourd;
+
+	cont += &first;
+	cont += &second;
+	cont += &third;
+	cont += &thourd;
+
+	std::cout << cont << std::endl;
+
+	cont.sort_by_content();
+
+	if (cont.check_sort_content())
+		std::cout << "Array is sorted by content." << std::endl;
+
+	else
+		std::cout << "Array has not sorted element" << std::endl;
 
 	return 0;
 }

972dd29a9e1dd3d649ce7ec67a95515a353afab3
Petr Kolesov <petrkolesov010204@gmail.com>
2024-12-29 13:30:01 +0700

    defined(constructors/destructor)
    * Constructor with paramerts (copy and default [size = 1] )

--- OOP_Lab3_template_class_vector/TVector.h
+++ OOP_Lab3_template_class_vector/TVector.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#ifndef ARE_TEMPLATE_VECTOR_DECLARED
+#define ARE_TEMPLATE_VECTOR_DECLARED
+
 template <typename Type>
 class TVector
 {
@@ -11,9 +14,6 @@ public:
 	TVector(const TVector<Type>&);
 	~TVector();
 
-
-
-
 private:
 	Type* m_vector = nullptr;
 	int m_size = 0;
@@ -24,3 +24,7 @@ std::ostream& operator << (std::ostream&, const TVector<Type>&);
 
 template <typename Type>
 std::istream& operator >> (std::istream&, TVector<Type>&);
+
+#include "TVector.cpp"
+
+#endif //!ARE_TEMPLATE_ARRAY_DECLARED

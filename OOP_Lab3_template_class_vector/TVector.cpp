38c2d9759a226daa5996da7d3eb5c30061123e9c
Petr Kolesov <petrkolesov010204@gmail.com>
2024-12-29 14:39:54 +0700

    add(new feature) : operators and method set length vector
    * defined operator >> and <<
    * defined operator []
    * defined operator = and (+= for vectors same length
    * initial method setLength - change length of vector

--- OOP_Lab3_template_class_vector/TVector.cpp
+++ OOP_Lab3_template_class_vector/TVector.cpp
@@ -1,5 +1,5 @@
-#ifndef ARE_TEMPLATE_VECTOR_METHODS_DEFINED
-#define ARE_TEMPLATE_VECTOR_METHODS_DEFINED
+#ifndef TEMPLATE_TVECTOR
+#define TEMPLATE_TVECTOR
 
 #include <iostream>
 #include <assert.h>
@@ -28,6 +28,96 @@ template <typename Type>
 TVector<Type>::~TVector()
 {
 	delete[]m_vector;
+}
+
+template <typename Type>
+Type& TVector<Type>::operator [] (const int index) const
+{
+	assert(index >= 0 && index < m_size);
+	return m_vector[index];
+}
+
+template <typename Type>
+TVector<Type>& TVector<Type>::operator = (const TVector<Type>& object)
+{
+	if (this == &object) {
+		return *this;
+	}
+
+	if (this->m_size != object.m_size) {
+		delete[] m_vector;
+
+		m_size = object.m_size;
+		m_vector = new Type[m_size];
+
+	}
+
+	for (int i = 0; i < m_size; i++) {
+		m_vector[i] = object.m_vector[i];
+	}
+
+	return *this;
+
+}
+
+template <typename Type>
+TVector<Type>& TVector<Type>::operator += (const TVector<Type>& object)
+{
+	if (m_size != object.m_size) {
+		return *this;
+	}
+
+	for (int i = 0; i < m_size; i++)
+		m_vector[i] += object.m_vector[i];
+
+	return *this;
+}
+
+template<typename Type>
+void TVector<Type>::setLength(int newSize)
+{
+	if (m_size = newSize)
+		return;
+	
+	// rewrite existing elements in vector
+	int minSize = std::min(m_size, newSize);
+	Type* tempVector = new Type[minSize];
+
+	for (int i = 0; i < minSize; i++)
+		tempVector[i] = m_vector[i];
+
+	delete[]m_vector;
+
+	m_vector = new Type[newSize]();
+
+	for (int i = 0; i < minSize; i++)
+		m_vector[i] = tempVector[i];
+
+	delete[]tempVector;
+
+	m_size = newSize;
+}
+
+template <typename Type>
+std::ostream& operator << (std::ostream& stream, const TVector<Type>& object)
+{
+	stream << "[ ";
+	for (int i = 0; i < object.m_size; i++)
+		stream << object[i] << " ";
+	stream << "]";
+
+	return stream;
+}
+
+template <typename Type>
+std::istream& operator >> (std::istream& stream, TVector<Type>& object)
+{
+	for (int i = 0; i < object.m_size; i++) {
+		std::cout << "[" << i << "] = ";
+		stream >> object[i];
+	}
+
+	return stream;
 }
 
 #endif
--- OOP_Lab3_template_class_vector/TVector.h
+++ OOP_Lab3_template_class_vector/TVector.h
@@ -1,17 +1,29 @@
 #pragma once
 
+#ifndef ARE_TEMPLATE_VECTOR_DECLARED
+#define ARE_TEMPLATE_VECTOR_DECLARED
+
 template <typename Type>
 class TVector
 {
+	template <typename Type>
 	friend std::ostream& operator << (std::ostream& stream, const TVector<Type>& object);
+	
+	template <typename Type>
 	friend std::istream& operator >> (std::istream& stream, TVector<Type>& object);
 
 public:
 
-	TVector(int size = 1);
-	TVector(const TVector<Type>&);
+	TVector( int size = 1 );
+	TVector( const TVector<Type>& );
 	~TVector();
 
+	Type& operator [] ( const int ) const;
+	TVector<Type>& operator = ( const TVector<Type>& );
+
+	TVector<Type>& operator += ( const TVector<Type>& );
+
+	void setLength( int );
 
 private:
 	Type* m_vector = nullptr;
@@ -23,3 +35,7 @@ std::ostream& operator << (std::ostream& stream, const TVector<Type>& object);
 
 template <typename Type>
 std::istream& operator >> (std::istream& stream, TVector<Type>& object);
+
+#include "TVector.cpp"
+
+#endif //!ARE_TEMPLATE_VECTOR_DECLARED

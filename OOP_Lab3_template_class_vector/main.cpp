254586d294caf023eac981aa17e95edd0c8e5871
Petr Kolesov <petrkolesov010204@gmail.com>
2024-12-29 14:40:13 +0700

    usage exmaple

--- OOP_Lab3_template_class_vector/main.cpp
+++ OOP_Lab3_template_class_vector/main.cpp
@@ -5,5 +5,16 @@
 int main() {
 	std::cout << "Hello world!" << std::endl;
 
+	TVector<int> object(5);
+	std::cin >> object;
+	std::cout << object << std::endl;
+
+	object.setLength(6);
+	std::cout << object << std::endl;
+	object.setLength(2);
+	std::cout << object << std::endl;
+	object.setLength(5);
+	std::cout << object << std::endl;
+
 	return 0;
 }
